d169070757a29029dc95d84915db4a781c1f1685
Gontzal Monasterio <go.monasterio@gmail.com>
2021-05-12 01:58:19 +0200

    add some fancy widgets

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -24,69 +24,156 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from typing import List  # noqa: F401
 
+import platform
 from libqtile import bar, layout, widget
-from libqtile.config import Click, Drag, Group, Key, Screen
+from libqtile.config import Group, Screen
+from libqtile.config import EzKey as Key
 from libqtile.lazy import lazy
 
-MOD = "mod1"
+# VARIABLES
 TERMINAL = "alacritty"
-
-# Challenger Deep Colors
 COLORS = {
-    "background":     "1e1c31",
-    "foreground":     "cbe1e7",
-    "black":          "141228",
-    "red":            "ff5458",
-    "green":          "62d196",
-    "yellow":         "ffb378",
-    "blue":           "65b2ff",
-    "magenta":        "906cff",
-    "cyan":           "63f2f1",
-    "white":          "a6b3cc",
-    "bright_black":   "565575",
-    "bright_red":     "ff8080",
-    "bright_green":   "95ffa4",
-    "bright_yellow":  "ffe9aa",
-    "bright_blue":    "91ddff",
+    "background": "1e1c31",
+    "foreground": "cbe1e7",
+    "black": "141228",
+    "red": "ff5458",
+    "green": "62d196",
+    "yellow": "ffb378",
+    "blue": "65b2ff",
+    "magenta": "906cff",
+    "cyan": "63f2f1",
+    "white": "a6b3cc",
+    "bright_black": "565575",
+    "bright_red": "ff8080",
+    "bright_green": "95ffa4",
+    "bright_yellow": "ffe9aa",
+    "bright_blue": "91ddff",
     "bright_magenta": "c991e1",
-    "bright_cyan":    "aaffe4",
-    "bright_white":   "cbe3e7",
+    "bright_cyan": "aaffe4",
+    "bright_white": "cbe3e7",
+    "pure_white": "ffffff",
+}
+MONAD_CONFIG = {
+    "border_focus": COLORS["blue"],
+    "border_width": 2,
+    "margin": 5,
+}
+WIDGETS_CONFIG = {
+    "font": "Fira Code Nerd Font",
+    "fontsize": 12,
+    "foreground": COLORS["pure_white"],
+    "padding": 5,
+}
+CURR_SCREEN_WIDGET_CONFIG = {
+    "active_color": COLORS["bright_green"],
+    "active_text": "\uf2d0",
+    "inactive_color": COLORS["bright_red"],
+    "inactive_text": "\uf2d4",
+}
+GROUP_BOX_WIDGET_CONFIG = {
+    "active": COLORS["pure_white"],
+    "borderwidth": 4,
+    "center_aligned": True,
+    "disable_drag": True,
+    "fontsize": 14,
+    "highlight_method": "line",
+    "inactive": COLORS["bright_black"],
+    "margin_x": 0,
+    "margin_y": 5,
+    "other_current_screen_border": COLORS["bright_white"],
+    "other_screen_border": COLORS["bright_white"],
+    "padding_x": 7,
+    "rounded": False,
+    "this_current_screen_border": COLORS["cyan"] ,
+    "this_screen_border": COLORS["cyan"],
+}
+PROMPT_WIDGET_CONFIG = {
+    "cursor_color": COLORS["cyan"],
+    "foreground": COLORS["cyan"],
+    "prompt": "\uf120  "
+}
+LAYOUT_WIDGET_CONFIG = {
+    "foreground": COLORS["bright_green"],
+    "fmt": "\uf247  {}",
+}
+UPDATES_WIDGET_CONFIG = {
+    "colour_have_updates": COLORS["bright_red"],
+    "colour_no_updates": COLORS["pure_white"],
+    "distro": "Arch_checkupdates",
+    "display_format": "\uf079 {updates}",
+    "no_update_string": "\uf079",
+    "update_interval": 60,
+}
+CPU_WIDGET_CONFIG = {
+    "format": "\uf2db {freq_current} GHz \uf0e4  {load_percent} %",
+    "foreground": COLORS["bright_magenta"],
+}
+CPU_TEMP_WIDGET_CONFIG = {
+    "fmt": "\uf2c9 CPU {}",
+    "foreground": COLORS["bright_yellow"],
+    "tag_sensor": "Tdie",
+}
+GPU_TEMP_WIDGET_CONFIG = {
+    "fmt": "GPU {}",
+    "foreground": COLORS["bright_yellow"],
+    "tag_sensor": "edge",
+}
+CLOCK_WIDGET_CONFIG = {
+    "foreground": COLORS["bright_blue"],
+    "format": "%Y-%m-%d %a %H:%M",
+}
+SEP_CONFIG = {
+    "linewidth": 2,
+    "size_percent": 50,
+}
+BAR_CONFIG = {
+    "background": COLORS["background"],
+    "opacity": 0.9,
 }
 
-# Keybindings
+# KEYBINDINGS
 keys = [
-    # Qtile
-    Key([MOD], "h", lazy.layout.left(),
-        desc="Move focus left in stack pane"),
-    Key([MOD], "j", lazy.layout.down(),
-        desc="Move focus down in stack pane"),
-    Key([MOD], "k", lazy.layout.up(),
-        desc="Move focus up in stack pane"),
-    Key([MOD], "l", lazy.layout.right(),
-        desc="Move focus right in stack pane"),
-    Key([MOD, "shift"], "j", lazy.layout.shuffle_down(),
-        desc="Move window down in current stack "),
-    Key([MOD, "shift"], "k", lazy.layout.shuffle_up(),
-        desc="Move window up in current stack "),
-    Key([MOD], "Return", lazy.spawn(TERMINAL), desc="Launch terminal"),
-    Key([MOD], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([MOD], "w", lazy.window.kill(), desc="Kill focused window"),
-    Key([MOD, "shift"], "r", lazy.restart(), desc="Restart qtile"),
-    Key([MOD, "shift"], "q", lazy.shutdown(), desc="Shutdown qtile"),
-    Key([MOD], "r", lazy.spawncmd(),
-        desc="Spawn a command using a prompt widget"),
+    # Qtile bindings
+    Key("A-h", lazy.layout.left(), desc="Move focus left in stack pane"),
+    Key("A-j", lazy.layout.down(), desc="Move focus down in stack pane"),
+    Key("A-k", lazy.layout.up(), desc="Move focus up in stack pane"),
+    Key("A-l", lazy.layout.right(), desc="Move focus right in stack pane"),
+    Key(
+        "A-S-h",
+        lazy.layout.shuffle_left(),
+        desc="Move window down in current stack "
+    ),
+    Key(
+        "A-S-j",
+        lazy.layout.shuffle_down(),
+        desc="Move window down in current stack "
+    ),
+    Key(
+        "A-S-k",
+        lazy.layout.shuffle_up(),
+        desc="Move window up in current stack "
+    ),
+    Key(
+        "A-S-l",
+        lazy.layout.shuffle_right(),
+        desc="Move window up in current stack "
+    ),
+    Key("A-<Return>", lazy.spawn(TERMINAL), desc="Launch terminal"),
+    Key("A-<Tab>", lazy.next_layout(), desc="Toggle between layouts"),
+    Key("A-w", lazy.window.kill(), desc="Kill focused window"),
+    Key("A-S-r", lazy.restart(), desc="Restart qtile"),
+    Key("A-r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
 
     # Personal bindings
-    Key([], "Print",
+    Key("<Print>",
         lazy.spawn("scrot -s /tmp/screenshot.png -e "
                    "'xclip -selection c -t image/png < $f'"),
-        desc="Launch scrot and copy screenshot to clipboard"
-    )
+        desc="Launch scrot and copy screenshot to clipboard",
+    ),
 ]
 
-# Groups
+# GROUPS
 group_list = [
     ("\uf0ac", {"layout": "max"}),
     ("\uf085", {"layout": "monadtall"}),
@@ -101,151 +188,114 @@ groups = [Group(name, **kwargs) for name, kwargs in group_list]
 
 for i, group in enumerate(groups, 1):
     keys.extend([
-        # Switch to another group
-        Key([MOD], str(i), lazy.group[group.name].toscreen(),
-            desc=f"Switch to group {group.name}"),
-
-        # Send to another group
-        Key([MOD, "shift"], str(i), lazy.window.togroup(group.name),
-            desc=f"Switch to & move focused window to group {group.name}"),
+        Key(
+            f"A-{i}",
+            lazy.group[group.name].toscreen(),
+            desc=f"Switch to group {group.name}"
+        ),
+        Key(
+            f"A-S-{i}",
+            lazy.window.togroup(group.name),
+            desc=f"Switch to & move focused window to group {group.name}"
+        ),
     ])
 
-# Layouts
-monad_config = {
-    "border_focus": COLORS["cyan"],
-    "border_width": 2,
-    "margin": 5,
-}
-
+# LAYOUTS
 layouts = [
-    layout.MonadTall(**monad_config),
-    layout.MonadWide(**monad_config),
+    layout.MonadTall(**MONAD_CONFIG),
+    layout.MonadWide(**MONAD_CONFIG),
     layout.Max(),
     layout.Matrix()
 ]
 
-# Widgets
-widget_defaults = {
-    "font": "Fira Code Nerd Font",
-    "fontsize": 12,
-    "foreground": COLORS["foreground"],
-    "padding": 5,
-}
+# WIDGETS
+widget_defaults = WIDGETS_CONFIG
+extension_defaults = WIDGETS_CONFIG
+SEP = widget.Sep(**SEP_CONFIG)
+SPACER = widget.Spacer(length=4)
+CLOCK = widget.Clock(**CLOCK_WIDGET_CONFIG)
 
-extension_defaults = widget_defaults.copy()
+# BARS AND SCREENS
+if platform.node() == "archlinux":
+    # BARS
+    primary_bar = bar.Bar(
+        [
+            widget.CurrentScreen(**CURR_SCREEN_WIDGET_CONFIG),
+            SPACER,
+            widget.GroupBox(**GROUP_BOX_WIDGET_CONFIG),
+            widget.Prompt(**PROMPT_WIDGET_CONFIG),
+            widget.WindowName(),
+            widget.CurrentLayout(**LAYOUT_WIDGET_CONFIG),
+            SEP,
+            widget.CheckUpdates(**UPDATES_WIDGET_CONFIG),
+            SEP,
+            widget.CPU(**CPU_WIDGET_CONFIG),
+            SEP,
+            widget.ThermalSensor(**CPU_TEMP_WIDGET_CONFIG),
+            widget.ThermalSensor(**GPU_TEMP_WIDGET_CONFIG),
+            SEP,
+            widget.Systray(),
+            # widget.Memory(),
+            CLOCK,
+        ],
+        30,
+        **BAR_CONFIG,
+    )
 
-curr_screen_config = {
-    "active_color": COLORS["bright_green"],
-    "active_text": "\uf2d0",
-    "inactive_color": COLORS["bright_red"],
-    "inactive_text": "\uf2d4",
-}
+    secondary_bar = bar.Bar(
+        [
+            widget.CurrentScreen(**CURR_SCREEN_WIDGET_CONFIG),
+            SPACER,
+            widget.GroupBox(**GROUP_BOX_WIDGET_CONFIG),
+            widget.WindowName(),
+            CLOCK,
+        ],
+        30,
+        **BAR_CONFIG,
+    )
 
-group_box_config = {
-    "active": COLORS["cyan"],
-    "borderwidth": 4,
-    "disable_drag": True,
-    "fontsize": 14,
-    "highlight_method": "line",
-    "inactive": COLORS["bright_black"],
-    "margin_x": 0,
-    "margin_y": 5,
-    "other_current_screen_border": COLORS["background"],
-    "other_screen_border": COLORS["background"],
-    "padding_x": 5,
-    "rounded": False,
-    "this_current_screen_border": COLORS["bright_yellow"] ,
-    "this_screen_border": COLORS["bright_yellow"],
-}
+    # SCREENS
+    screens = [
+        Screen(
+            top=primary_bar,
+        ),
+        Screen(
+            top=secondary_bar,
+        ),
+    ]
+else: # miair13
+    # BARS
+    bar = bar.Bar(
+        [
+            widget.CurrentScreen(**CURR_SCREEN_WIDGET_CONFIG),
+            SPACER,
+            widget.GroupBox(**GROUP_BOX_WIDGET_CONFIG),
+            widget.Prompt(**PROMPT_WIDGET_CONFIG),
+            widget.WindowName(),
+            widget.CurrentLayout(**LAYOUT_WIDGET_CONFIG),
+            SEP,
+            widget.CheckUpdates(**UPDATES_WIDGET_CONFIG),
+            SEP,
+            widget.CPU(**CPU_WIDGET_CONFIG),
+            SEP,
+            widget.Systray(),
+            widget.BatteryIcon(),
+            SEP,
+            widget.Wlan(),
+            # widget.Memory(),
+            CLOCK,
+        ],
+        30,
+        **BAR_CONFIG,
+    )
 
-layout_config = {
-    "foreground": COLORS["magenta"],
-}
+    # SCREENS
+    screens = [
+        Screen(
+            top=bar,
+        ),
+    ]
 
-updates_config = {
-    "colour_have_updates": COLORS["bright_red"],
-    "colour_no_updates": COLORS["bright_green"],
-    "distro": "Arch_checkupdates",
-    "display_format": "\uf079 {updates}",
-    "no_update_string": "\uf079 0",
-    "update_interval": 60,
-}
-
-# key_layout_config = {
-    # "configured_keyboards": ["us", "es"],
-    # "foreground": COLORS["blue"],
-    # "option": "grp:lwin_toggle",
-# }
-
-clock_config = {
-    "foreground": COLORS["blue"],
-    "format": "%Y-%m-%d %a %H:%M",
-}
-
-# Screens
-bar_config = {
-    "background": COLORS["background"],
-    "opacity": 0.9,
-}
-
-primary_bar = bar.Bar(
-    [
-        widget.CurrentScreen(**curr_screen_config),
-        widget.Spacer(length=4),
-        widget.GroupBox(**group_box_config),
-        widget.Prompt(),
-        widget.WindowName(),
-        widget.CurrentLayout(**layout_config),
-        widget.Sep(height_percent=60),
-        widget.CheckUpdates(**updates_config),
-        widget.Sep(height_percent=60),
-        # widget.Sep(height_percent=60),
-        # widget.KeyboardLayout(),
-        widget.Systray(),
-        # widget.CPU(),
-        # widget.Memory(),
-        widget.Clock(**clock_config),
-        # widget.CPUGraph(),
-    ],
-    30,
-    **bar_config,
-)
-
-secondary_bar = bar.Bar(
-    [
-        widget.CurrentScreen(**curr_screen_config),
-        widget.Spacer(length=4),
-        widget.GroupBox(**group_box_config),
-        widget.WindowName(),
-        widget.Clock(**clock_config),
-    ],
-    30,
-    **bar_config,
-)
-
-screens = [
-    Screen(
-        top=primary_bar,
-    ),
-    Screen(
-        top=secondary_bar,
-    ),
-]
-
-# Drag floating layouts.
-mouse = [
-    Drag([MOD], "Button1", lazy.window.set_position_floating(),
-         start=lazy.window.get_position()),
-    Drag([MOD], "Button3", lazy.window.set_size_floating(),
-         start=lazy.window.get_size()),
-    Click([MOD], "Button2", lazy.window.bring_to_front())
-]
-
-dgroups_key_binder = None
-dgroups_app_rules = []  # type: List
-follow_mouse_focus = True
-bring_front_click = False
-cursor_warp = False
 floating_layout = layout.Floating(float_rules=[
     # Run the utility of `xprop` to see the wm class and name of an X client.
     {'wmclass': 'confirm'},
@@ -263,6 +313,4 @@ floating_layout = layout.Floating(float_rules=[
     {'wname': 'pinentry'},  # GPG key password entry
     {'wmclass': 'ssh-askpass'},  # ssh-askpass
 ])
-auto_fullscreen = True
-focus_on_window_activation = "smart"
 wmname = "Qtile"
