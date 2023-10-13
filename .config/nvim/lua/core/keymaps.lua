66f483026fd74fbdb22c97b4c16e5cbad62af13e
gontzalm <go.monasterio@gmail.com>
2023-10-13 23:46:59 +0200

    Remove unnecessary keymap

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,6 +1,5 @@
 vim.g.mapleader = ","
 vim.g.maplocalleader = ","
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<space>", "za")
 vim.keymap.set("n", "<c-h>", "<c-w>h")
 vim.keymap.set("n", "<c-j>", "<c-w>j")
