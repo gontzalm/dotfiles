745e914db6c4b362462bfac9fdb5c8f70f3b9f6a
gontzalm <go.monasterio@gmail.com>
2023-10-13 23:45:55 +0200

    Configure prettierd

--- .config/nvim/lua/plugins/guard.lua
+++ .config/nvim/lua/plugins/guard.lua
@@ -1,49 +1,48 @@
 return {
-    "gontzalm/guard.nvim",
+    "nvimdev/guard.nvim",
     dependencies = { "nvimdev/guard-collection" },
     event = { "BufReadPre", "BufNewFile" },
     config = function()
         local ft = require("guard.filetype")
-        local lint = require("guard.lint")
+        -- local lint = require("guard.lint")
 
-        local prettierd = {
+        ft("html,json,jsonc,yaml,markdown"):fmt({
             cmd = "prettierd",
             fname = true,
             stdin = true,
-        }
-
-        ft("html,json,jsonc,yaml"):fmt(prettierd)
-
-        -- ft("lua"):fmt("lsp")
-
-        ft("markdown"):fmt(prettierd):lint({
-            cmd = "markdownlint",
-            args = { "--json" },
-            fname = true,
-            valid_exit_codes = { 0, 1 },
-            source = "markdownlint",
-            parse = lint.from_json({
-                source = "markdownlint",
-                attributes = {
-                    lnum = "lineNumber",
-                    col = "lineNumber",
-                    message = "ruleDescription",
-                    code = function(d) return d["ruleNames"][1] end,
-                    severity = function(_) return "warning" end,
-                }
-            })
+            env = { PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/.config/nvim/linter-config/.prettierrc.toml") },
         })
 
-        ft("sh"):fmt({
-            cmd = "shfmt",
-            args = {"-i", "2", "-ci", "-bn"},
-            stdin = "true"
-        }):lint("shellcheck")
+        ft("lua"):fmt("lsp")
+
+        -- ft("markdown"):lint({
+        --     cmd = "markdownlint",
+        --     args = { "--json" },
+        --     fname = true,
+        --     valid_exit_codes = { 0, 1 },
+        --     source = "markdownlint",
+        --     parse = lint.from_json({
+        --         source = "markdownlint",
+        --         attributes = {
+        --             lnum = "lineNumber",
+        --             col = "lineNumber",
+        --             message = "ruleDescription",
+        --             code = function(d) return d["ruleNames"][1] end,
+        --             severity = function(_) return "warning" end,
+        --         }
+        --     })
+        -- })
 
         ft("python"):fmt({
             cmd = "blackd-client",
             stdin = true,
         })
+
+        ft("sh"):fmt({
+            cmd = "shfmt",
+            args = { "-i", "2", "-ci", "-bn" },
+            stdin = "true"
+        }):lint("shellcheck")
 
         ft("toml"):fmt("lsp")
 
