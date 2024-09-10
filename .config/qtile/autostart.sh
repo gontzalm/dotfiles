8af357401de42e9a412c7b00dae5799504704cce
gontzalm <go.monasterio@gmail.com>
2024-09-10 22:36:13 +0200

    migrate to hyprland

--- .config/qtile/autostart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-kanshi &
-systemctl --user import-environment
--- .config/qtile/config_variables/groups.py
+++
@@ -1,6 +0,0 @@
-from config_variables._helpers import GroupsGenerator
-from config_variables.conf import Config
-
-groups_generator = GroupsGenerator(Config.GROUP_LIST)
-groups = groups_generator.generate_groups()
-group_keys = groups_generator.generate_keybindings()
