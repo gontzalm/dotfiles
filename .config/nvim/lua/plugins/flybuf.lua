b0d9f73d61db49ee7b2d15352d991b79aca02e1c
Gontzal Monasterio <go.monasterio@gmail.com>
2024-09-18 15:49:36 +0200

    configure telescope

--- .config/nvim/lua/plugins/flybuf.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "nvimdev/flybuf.nvim",
-    keys = {
-        { "<leader>fb", vim.cmd.FlyBuf, desc = "Open FlyBuf." }
-    },
-    config = true,
-}
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,27 +3,43 @@ return {
     tag = "0.1.3",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
+        -- File pickers
         {
             "<leader>ff",
-            function() require("telescope.builtin").find_files() end,
+            function()
+                if vim.fn.isdirectory(".git") ~= 0 then
+                    require("telescope.builtin").git_files()
+                else
+                    require("telescope.builtin").find_files()
+                end
+            end,
             desc = "Find files."
         },
         {
             "<leader>fg",
-            function()
-                if vim.loop.fs_stat(".git") then
-                    require("telescope.builtin").git_files()
-                else
-                    require("telescope.builtin").live_grep()
-                end
-            end,
+            require("telescope.builtin").live_grep,
             desc = "Grep files."
+        },
+        -- Vim pickers
+        {
+            "<leader>fb",
+            function() require("telescope.builtin").buffers({ initial_mode = "normal" }) end,
+            desc = "List open buffers."
+        },
+        {
+            "<leader>fc",
+            require("telescope.builtin").commands,
+            desc = "Find commands."
+        },
+        {
+            "q:",
+            require("telescope.builtin").command_history,
+            desc = "Find help tags."
         },
         {
             "<leader>fh",
-            function() require("telescope.builtin").help_tags() end,
+            require("telescope.builtin").help_tags,
             desc = "Find help tags."
         }
-
     }
 }
