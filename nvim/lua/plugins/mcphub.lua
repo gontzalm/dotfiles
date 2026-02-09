14f1eeba6d5a30d6757d0b902a4b0b5e21c66654
Gontzal Monasterio <go.monasterio@gmail.com>
2026-02-09 14:45:13 +0100

    delete mcphub

--- nvim/lua/plugins/mcphub.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "ravitemer/mcphub.nvim",
-    build = "npm install -g mcp-hub@latest",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    cmd = { "MCPHub" },
-    opts = { auto_approve = true }
-}
